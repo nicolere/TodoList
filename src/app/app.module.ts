137a8c39d950c0fdcfa36d12bb6179f08f125e98
Nicolas <nicolas.gauss.1@orange.fr>
2019-10-28 18:35:47 +0100

    import fontAwesome module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { AppComponent } from "./app.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { TodoItemComponent } from "./todo-item/todo-item.component";
+import { TodoService } from "./todo.service";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-import {TodoService} from './todo.service';
-import {FormsModule} from '@angular/forms';
-
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoItemComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+  imports: [BrowserModule, FormsModule, FontAwesomeModule],
   providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

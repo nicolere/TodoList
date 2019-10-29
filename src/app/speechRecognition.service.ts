4ac2deff7449e37c14b642845afc264a3ad82801
Nicolas <nicolas.gauss.1@orange.fr>
2019-10-29 15:52:33 +0100

    Comments

--- src/app/speechRecognition.service.ts
+++ src/app/speechRecognition.service.ts
@@ -17,19 +17,26 @@ export class SpeechRecognitionService {
     return Observable.create(observer => {
       const { webkitSpeechRecognition }: IWindow = <IWindow>(<any>window);
       this.speechRecognition = new webkitSpeechRecognition();
+
+      // Autorise l'écoute en continue
       this.speechRecognition.continuous = false;
       this.speechRecognition.interimresults = true;
+
+      // Choisi la langue d'écoute
       this.speechRecognition.lang = "fr";
 
+      // Ecoute
       this.speechRecognition.onresult = speech => {
         let term: string = "";
         if (speech.results) {
+          // Retranscription du vocal
           var result = speech.results[speech.resultIndex];
           var transcript = result[0].transcript;
           if (result.isFinal) {
             if (result[0].confidence < 0.3) {
               console.log("Résultat non-reconnu -- Réessayez");
             } else {
+              // Convertit en string la retranscription
               term = _.trim(transcript);
               //   console.log("vous avez dit : "+ term);
             }
@@ -39,9 +46,11 @@ export class SpeechRecognitionService {
           observer.next(term);
         });
       };
+      // Renvoie vers la partie erreur
       this.speechRecognition.onerror = error => {
         observer.error(error);
       };
+      // Renvoie vers la partie complete
       this.speechRecognition.onend = () => {
         observer.complete();
       };

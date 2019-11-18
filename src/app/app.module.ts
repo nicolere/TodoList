19411210321c57cbbb8680d41ff204a8a097023a
Nicolas <nicolas.gauss.1@orange.fr>
2019-11-18 15:53:29 +0100

    import GeoService and AGm Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { TodoListComponent } from "./todo-list/todo-list.component";
 import { TodoItemComponent } from "./todo-item/todo-item.component";
+
 import { TodoService } from "./todo.service";
 import { SpeechRecognitionService } from "./speechRecognition.service";
-import { FormsModule } from "@angular/forms";
 
+import { FormsModule } from "@angular/forms";
+import { AgmCoreModule } from "@agm/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { GeocodeService } from "./geocode.service";
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoItemComponent],
-  imports: [BrowserModule, FormsModule, FontAwesomeModule],
-  providers: [TodoService, SpeechRecognitionService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyC4hIYHPuggHPkKR8FeZt4ck5ArCvIy0YA"
+    })
+  ],
+  providers: [TodoService, SpeechRecognitionService, GeocodeService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

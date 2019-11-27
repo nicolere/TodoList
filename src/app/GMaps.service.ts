5a0c9a70a89507c9b3b95adf10fd048b4ce6ef57
Nicolas <nicolas.gauss.1@orange.fr>
2019-11-27 18:22:45 +0100

    update GMService to Promise

--- src/app/GMaps.service.ts
+++ src/app/GMaps.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from "@angular/core";
 import { MapsAPILoader } from "@agm/core";
-import { Observable } from "rxjs";
-import { Subject } from "rxjs/Subject";
-import { of } from "rxjs/observable/of";
-import { fromPromise } from "rxjs/observable/fromPromise";
-import { catchError, tap, map, switchMap } from "rxjs/operators";
 import { Location } from "./location-model";
 
 declare var google: any;
@@ -20,23 +15,28 @@ export class GMapsService {
     this.geocoder = new google.maps.Geocoder();
   }
 
-  geocodeAddress(location: string): Observable<Location> {
+  geocodeAddress(location: string): Promise<Location> {
     this.initGeocoder();
     console.log("Start Geocoding of - ", location);
-    return new Observable(observer => {
-      this.geocoder.geocode({ address: location }, (results, status) => {
-        if (status == google.maps.GeocoderStatus.OK) {
-          console.log("Geocoding complete!");
-          observer.next({
-            lat: results[0].geometry.location.lat(),
-            lng: results[0].geometry.location.lng()
-          });
-        } else {
-          console.log("Error - ", results, " & Status - ", status);
-          observer.next({ lat: 0, lng: 0 });
-        }
-        observer.complete();
-      });
-    });
+    return new Promise(
+      function(resolve, reject) {
+        this.geocoder.geocode({ address: location }, (results, status) => {
+          if (status == google.maps.GeocoderStatus.OK) {
+            console.log("Geocoding complete!");
+            const obj: Location = {
+              lat: results[0].geometry.location.lat(),
+              lng: results[0].geometry.location.lng(),
+              city: location
+            };
+            // Resolve ...
+            resolve(obj);
+          } else {
+            console.log("Error - ", results, " & Status - ", status);
+            // Reject ...
+            reject(status);
+          }
+        });
+      }.bind(this)
+    );
   }
 }
